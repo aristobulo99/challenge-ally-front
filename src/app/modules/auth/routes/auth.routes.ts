2f5555c3b16d864b7c3923fa145bbc9851a2b775
juniorespriella99 <juniorespriella99@gmail.com>
2025-06-18 20:26:25 -0500

    feat: integración de ruta y vista login (inicio de sesión)

--- src/app/modules/auth/routes/auth.routes.ts
+++ src/app/modules/auth/routes/auth.routes.ts
@@ -4,6 +4,16 @@ import { AuthLayoutComponent } from "../layout/auth-layout/auth-layout.component
 export const AuthRoutes: Routes = [
     {
         path: 'auht',
-        component: AuthLayoutComponent
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: 'login',
+                loadComponent: () => import('../views/login/login.component').then(m => m.LoginComponent)
+            },
+            {
+                path: '**',
+                redirectTo: 'login',
+            }
+        ]
     }
 ]
