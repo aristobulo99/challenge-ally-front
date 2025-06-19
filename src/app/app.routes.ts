d05d7f1cf1555738015295f2c87c394891864288
juniorespriella99 <juniorespriella99@gmail.com>
2025-06-18 20:14:44 -0500

    feat: integración de layout y configuración de routes de sección de autentificación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AuthRoutes } from './modules/auth/routes/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...AuthRoutes,
+    {
+    path: '**',
+    redirectTo: '/auht',
+    pathMatch:'full'
+  }
+];
---
+++ src/app/modules/auth/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { AuthLayoutComponent } from "../layout/auth-layout/auth-layout.component";
+
+export const AuthRoutes: Routes = [
+    {
+        path: 'auht',
+        component: AuthLayoutComponent
+    }
+]
